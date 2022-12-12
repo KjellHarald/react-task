21b75c340c04520c7f93784dd6e915084a087ff0
Kjell Harald <kjell.kodehode@gmail.com>
2022-12-12 09:39:15 +0100

    useEffect

--- src/Components/Content.js
+++ src/Components/Content.js
@@ -1,7 +1,27 @@
+import { useState, useEffect } from "react"
 const Content = () => {
+    const [apiInfo, setInfoApi] = useState([])
+
+    const someAPI = async () => {
+        const response = await fetch('https://pokeapi.co/api/v2/', {
+            "method":"GET"
+        })
+
+        const data = await response.json()
+        setInfoApi(data)
+        console.log('Call API')
+    }
+
+    useEffect(()=>{
+        someAPI()
+    }, [])
+    
     return (
         <div className="content">
             <p>Import/Export</p>
+            {Object.entries(apiInfo).map(([key, val])=>{
+                return (<p>{key} : {val}</p>)
+            })}
         </div>
     )
 }
